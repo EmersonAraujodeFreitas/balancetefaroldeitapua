ae6e3b9d550e1f7305afc2786f54d8b05107ec40
EmersonAraujodeFreitas <skylleres12@gmail.com>
2026-03-25 09:05:46 -0300

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,25 @@
+const API = "http://localhost:3000";
+
+async function enviar() {
+    const file = document.getElementById("file").files[0];
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const res = await fetch(API + "/upload", {
+        method: "POST",
+        body: formData
+    });
+
+    const data = await res.json();
+
+    const r = data.resultado;
+
+    document.getElementById("score").innerText = r.score;
+    document.getElementById("status").innerText = r.status;
+    document.getElementById("compras").innerText = r.comprasAltas.length;
+    document.getElementById("inconsistencias").innerText = r.inconsistencias.length;
+
+    document.getElementById("detalhes").innerText =
+        JSON.stringify(r, null, 2);
+}
